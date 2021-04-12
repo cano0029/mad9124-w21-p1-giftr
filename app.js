3a9a600143d3f7dafa867c244d98bf6cf125a863
Johanna Cano <cano0029@algonquinlive.com>
2021-04-12 17:00:51 -0400

    Set up app.js with dependencies

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 // Don't forget to use NPM to install all of the required dependencies
+import morgan from 'morgan'
 import express from 'express'
 const app = express();
 
+app.use(morgan('tiny'))
+app.use(express.json())
+
+// routes
+app.get('/', (req, res) => res.send('Hello'))
+
+export default app
