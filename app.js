c510cc241b93cc3c66ba90292f023de67ffb35bf
Johanna Cano <cano0029@algonquinlive.com>
2021-04-12 16:03:08 -0400

    Delete comments

--- app.js
+++ app.js
@@ -1,2 +1,1 @@
 // Don't forget to use NPM to install all of the required dependencies
-// Steve has a starter code for this: maybe go through it little by little instead of pasting it all at once here just to break it down
