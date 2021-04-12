0da7401bd87042eaef54b7b7325588627312be0e
Johanna Cano <cano0029@algonquinlive.com>
2021-04-12 19:36:32 -0400

    Get main app.js

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
 // Don't forget to use NPM to install all of the required dependencies
 import morgan from 'morgan'
 import express from 'express'
-import morgan from 'morgan'
 import connectDatabase from "./startup/connectDatabase.js"
 
 connectDatabase();
 const app = express();
 app.use(morgan('tiny'))
 app.use(express.json())
-
-export default app
-
-app.use(morgan('tiny'))
-app.use(express.json())
-
-// routes
-app.get('/', (req, res) => res.send('Hello'))
 
 export default app
