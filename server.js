2cd5423cc4b3ad91f5bc864e05d7005714aed395
Johanna Cano <cano0029@algonquinlive.com>
2021-04-12 17:16:14 -0400

    Set up server.js

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
 // You should separate the HTTP server from the Express app
-import http from 'http'
-import app from ('./app.js')
-
-const httpServer = http.createServer(app)
-
-const port = process.env.PORT || 3030
-httpServer.listen(port, () => {
-  console.log(`HTTP server is listening on port ${httpServer.address().port}`)
-})
