0e803fe8816ca696597bdc2e22cb138552b022ee
Eric Shantz <shan0255@algonquinlive.com>
2021-04-12 16:58:53 -0400

    add server

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 // You should separate the HTTP server from the Express app
+import http from 'http'
+import app from './app.js'
+import createDebug from 'debug'
+
+const debug = createDebug('maddemo:httpServer')
+
+const httpServer = http.createServer(app)
+
+const port = process.env.PORT || 3030
+httpServer.listen(port, () => {
+    console.log(`HTTP server listening on port ${port}`)
+    // debug(`HTTP server listening on port ${port}`) ... this doesnt working
+})
+
