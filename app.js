74ced31a5645e875c267c2a7f70e6a9ca2e84696
Johanna Cano <cano0029@algonquinlive.com>
2021-04-12 20:13:58 -0400

    Set up sanitization middleware

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 import morgan from 'morgan'
 import express from 'express'
 import connectDatabase from './startup/connectDatabase.js'
+import santizeBody from './middleware/sanitizeBody.js'
 
 connectDatabase()
 const app = express()
 app.use(morgan('tiny'))
 app.use(express.json())
+
+app.get('/', (req, res) => {})
+app.post('/test', sanitizeBody, (req, res) => {})
 
 export default app
