e2a910beccdda8cb7e282e559e5f571d462c3b60
Eric Shantz <shan0255@algonquinlive.com>
2021-04-13 16:11:22 -0400

    fix handleError middleware

--- routes/gift.js
+++ routes/gift.js
@@ -4,26 +4,16 @@ import sanitizeBody from '../middleware/sanitizeBody.js'
 import express from 'express'
 const router = express.Router()
 
-router.post('/', sanitizeBody, async (req, res) => {
-
-  let newDocument = new Gift(req.sanitizedBody)
-  // push to array?
-  // save to parent document (Person)?
-  try {
-      await newDocument.save()
-      res.status(201).send({ data: newDocument })
-  } catch (err) {
-      console.log(err)
-      res.status(500).send({
-          errors: [
-              {
-                  status: '500',
-                  title: 'Server error',
-                  description: 'Problem saving document to the database.',
-              },
-          ],
-      })
-  }
+router.post('/', sanitizeBody, async (req, res , next) => {
+    let newDocument = new Gift(req.sanitizedBody)
+    // push to array?
+    // save to parent document (Person)?
+    try {
+        await newDocument.save()
+        res.status(201).send({ data: newDocument })
+    } catch (error) {
+        next(error)
+    }
 })
 
 export default router
