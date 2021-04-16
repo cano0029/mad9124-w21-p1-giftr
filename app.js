bc9e75c17ee4d5bbc34b6df14a80ec08fa773315
Johanna Cano <cano0029@algonquinlive.com>
2021-04-16 11:32:26 -0400

    Apply compression as an app middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-// Don't forget to use NPM to install all of the required dependencies
 import morgan from 'morgan'
 import express from 'express'
+import cors from 'cors'
+import compression from 'compression'
 import connectDatabase from './startup/connectDatabase.js'
 import sanitizeMongo from 'express-mongo-sanitize'
 import peopleRouter from './routes/person.js'
@@ -9,21 +10,25 @@ import authRouter from './routes/auth/index.js'
 import handleErrors from './middleware/handleErrors.js'
 import logger from './startup/logger.js'
 
-
-
 connectDatabase()
+
 const app = express()
+
 logger.info(app.get('env')) // tells us in the terminal what ENV we are in - defaults to development if we do not set the ENV variable
 // run this command in the terminal to set to prod ENV... export NODE_ENV=production
 // run this command in the terminal to set to dev ENV... export NODE_ENV=development
+
+// middlewares
+app.use(cors())
+app.use(compression())
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use(sanitizeMongo())
 
+// routes
 app.use('/api/people', peopleRouter)
 app.use('/api/people', giftsRouter)
 app.use('/auth', authRouter)
 app.use(handleErrors)
-
 
 export default app
