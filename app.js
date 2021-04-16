e30700658901c61faa510025d8dcd7c5be8b24d1
Johanna Cano <cano0029@algonquinlive.com>
2021-04-16 10:42:52 -0400

    Set up deployment environment to production ENV

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ import peopleRouter from './routes/person.js'
 import giftsRouter from './routes/gift.js'
 import authRouter from './routes/auth/index.js'
 import handleErrors from './middleware/handleErrors.js'
+import logger from './startup/logger.js'
+
 
 
 connectDatabase()
 const app = express()
+logger.info(app.get('env')) // tells us in the terminal what ENV we are in - defaults to development if we do not set the ENV variable
+// run this command in the terminal to set to prod ENV... export NODE_ENV=production
+// run this command in the terminal to set to dev ENV... export NODE_ENV=development
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use(sanitizeMongo())
--- startup/logger.js
+++ startup/logger.js
@@ -18,7 +18,7 @@ const logger = winston.createLogger({
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') { // if it is in production, do not log it into console
   logger.add(
     new winston.transports.Console({
       format: winston.format.combine(
