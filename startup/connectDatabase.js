8ae48cff4c48d46e1f37733fab7000868d1120ce
Johanna Cano <cano0029@algonquinlive.com>
2021-04-16 21:11:41 -0400

    Connect mongoose to Atlas cluster

--- startup/connectDatabase.js
+++ startup/connectDatabase.js
@@ -3,21 +3,52 @@ import config from 'config'
 import logger from './logger.js'
 
 const log = logger.child({ module: 'connectDB' })
-const dbConfig = config.get('db')
+// const dbConfig = config.get('db')
 
-export default function connectDatabase() {
-    mongoose
-        .connect(`mongodb://${dbConfig.host}:${dbConfig.port}/${dbConfig.dbName}`, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true,
-        })
-        .then(() => {
-            log.info('Successfully connected to MongoDB ...')
-        })
-        .catch((err) => {
-            log.error('Error connecting to MongoDB ... ', err.message)
-            process.exit(1)
-        })
+// export default function connectDatabase() {
+//     mongoose
+//         .connect(`mongodb://${dbConfig.host}:${dbConfig.port}/${dbConfig.dbName}`, {
+//             useNewUrlParser: true,
+//             useCreateIndex: true,
+//             useFindAndModify: false,
+//             useUnifiedTopology: true,
+//         })
+//         .then(() => {
+//             log.info('Successfully connected to MongoDB ...')
+//         })
+//         .catch((err) => {
+//             log.error('Error connecting to MongoDB ... ', err.message)
+//             process.exit(1)
+//         })
+// }
+
+// AWS deployment  
+export default async function connectDatabase() {
+    const { scheme, host, port, name, username, password, authSource } = config.get('db')
+    const credentials = username && password ? `${username}:${password}@` : ''
+
+    let connectionString = `${scheme}://${credentials}${host}`
+
+    if (scheme === 'mongodb') {
+        connectionString += `:${port}/${name}?authSource=${authSource}`
+    } else {
+        connectionString += `/${authSource}?retryWrites=true&w=majority`
+    }
+
+    try {
+        await mongoose.connect(
+            connectionString,
+            {
+                useNewUrlParser: true,
+                useCreateIndex: true,
+                useFindAndModify: false,
+                useUnifiedTopology: true,
+                dbName: name
+            }
+        )
+    } catch(err) {
+        log.error(`Error connecting to MongoDB ...`, err)
+        process.exit(1)
+    }
 }
+
